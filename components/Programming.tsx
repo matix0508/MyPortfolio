3357a66a4d8cdd6a041895af2a2e2aedece57da7
Mateusz Wlekliński <wleklinskimateusz05@gmail.com>
2022-02-18 23:53:06 +0100

    add text

--- components/Programming.tsx
+++ components/Programming.tsx
@@ -4,15 +4,20 @@ import { Card } from "./Card";
 export const Programming = () => {
   return (
     <Card title="Programming">
-      <h4><a href="http://solarplane.agh.edu.pl/">AGH Solar Plane</a></h4>
-      <p>I am a member of AGH Solar Plane for a year. There are plenty of skills I aquiared here. Among them:</p>
-      <ul>
-        <li>Team Managment</li>
-        <li>Working with others</li>
-        <li>Front End framework (React.js)</li>
-        <li>Lots of other things related to building a plane</li>
-      </ul>
-      
+      <p>
+        I started my journey with coding in High I started my journey with
+        coding in High School. I programmed simple python games, scripts for
+        optimizing desktop tasks (such as automatically filter Downloads folder)
+      </p>
+      <p>
+        {" "}
+        As my experience grew, I started to do more ambitious tasks. I couldn't
+        decide on one thing for me to pursue.{" "}
+      </p>
+      <p>
+        Finally, I settled with the Web Development, and I'm doing my best to
+        master it in the future
+      </p>
     </Card>
   );
 };
