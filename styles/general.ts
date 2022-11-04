df454fee4aade261f3053bc6e054a9ffccdcb523
Mateusz Wlekliński <51704650+wleklinskimateusz@users.noreply.github.com>
2022-11-04 14:12:33 +0000

    setup storybook (#16)

--- styles/general.ts
+++ styles/general.ts
@@ -1,10 +1,17 @@
 import { darken } from "polished";
+import { css } from "styled-components";
 import { colors } from "./colors";
 
 export const transitionTime = "0.3s";
-export const shadow = `box-shadow: 0 1px 4px ${darken(0.3, colors.bg)}`;
-export const transition = `transition: all ${transitionTime} ease-out`;
-export const glow = `box-shadow: 0px 2px 6px ${colors.primary}`;
+export const shadow = css`
+  box-shadow: 0 1px 4px ${darken(0.3, colors.bg)};
+`;
+export const transition = css`
+  transition: all ${transitionTime} ease-out;
+`;
+export const glow = css`
+  box-shadow: 0px 2px 6px ${colors.primary};
+`;
 export const borderRadius = "8px";
 export const marginSize = {
   small: "0.5rem",
