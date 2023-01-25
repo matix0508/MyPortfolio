af0bce1e87579a3a076e4451caacd502cc5211c8
wleklinskimateusz <wleklinskimateusz05@gmail.com>
2023-01-25 09:24:08 +0100

    add schema api for skill

---
+++ schema/api/common.ts
@@ -0,0 +1,26 @@
+import z, { ZodRawShape } from "zod";
+
+export function getApiDataSingleElement<T extends ZodRawShape>(
+  data: z.ZodObject<T>
+) {
+  return z.object({
+    id: z.string(),
+    attributes: z
+      .object({
+        locale: z.string(),
+      })
+      .merge(data),
+  });
+}
+
+export function getApiDataSingle<T extends ZodRawShape>(data: z.ZodObject<T>) {
+  return z.object({
+    data: getApiDataSingleElement(data),
+  });
+}
+
+export function getApiDataMany<T extends ZodRawShape>(data: z.ZodObject<T>) {
+  return z.object({
+    data: z.array(getApiDataSingleElement(data)),
+  });
+}
